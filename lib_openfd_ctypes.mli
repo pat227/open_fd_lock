cd245c1d4b9e417f3da57264200447473be91544
paul <paul@guerriere>
2015-05-20 23:27:21 -0400

    new file:   README 	deleted:    lib_openfd_ctypes.ml 	deleted:    lib_openfd_ctypes.mli

--- lib_openfd_ctypes.mli
+++
@@ -1,8 +0,0 @@
-module OpenFDLocks :
-  sig
-    type flock64
-    val flock64 : flock64 Ctypes.structure Ctypes.typ
-    (*val openfdlockdl : Dl.library*)
-    val acquireLock : int -> flock64 Ctypes.structure Ctypes_static.ptr
-    val releaseLock : flock64 Ctypes.structure Ctypes_static.ptr -> int -> unit
-  end
